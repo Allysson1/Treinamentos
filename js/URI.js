1a7ec8a94016ad3162353871f981a07cd5761f81
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2022-02-04 17:29:01 -0300

    resolvendo dessafios do URI com js

---
+++ js/URI.js
@@ -0,0 +1,14 @@
+// 1001
+
+
+// var A, B, X;
+// A = prompt();
+// B = prompt();
+// A = parseInt(A);
+// B = parseInt(B);
+// X = A + B;
+// console.log("X = " + X);
+
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(function() { console.log(i); }, 1000+i);
+// 
