7b50abf245976c8e82b5c0d976cacffff2b8a9b7
Allysson1 <81646221+Allysson1@users.noreply.github.com>
2022-02-15 15:27:05 -0300

    conhecendo um pouco mais do console e destructuring assigment

--- js/ES6/es6.js
+++ js/ES6/es6.js
@@ -122,99 +122,154 @@
 
 //exemplo caso fosse solicitado algum vvalor e o usuário não o tivesse atribuido
 
-function multiply(a, b) {
-  b = typeof b === "undefined" ? 1 : b; // caso com validação de tipo
-  return a * b;
-}
-console.log(multiply(5));
+// function multiply(a, b) {
+//   b = typeof b === "undefined" ? 1 : b; // caso com validação de tipo
+//   return a * b;
+// }
+// console.log(multiply(5));
 
-//outtro exemplo do mesmo caso       \\ = ou
-function multiply2(a, b) {
-  b = b || 1;
-  return a * b;
-}
-console.log(multiply2(5));
+// //outtro exemplo do mesmo caso       \\ = ou
+// function multiply2(a, b) {
+//   b = b || 1;
+//   return a * b;
+// }
+// console.log(multiply2(5));
 
-//  mesmo caso com if
-function multiply3(a, b) {
-  if ((b = typeof b === "undefined")) {
-    b = 1;
-  }
-  return a * b;
-}
-console.log(multiply3(5));
+// //  mesmo caso com if
+// function multiply3(a, b) {
+//   if ((b = typeof b === "undefined")) {
+//     b = 1;
+//   }
+//   return a * b;
+// }
+// console.log(multiply3(5));
 
-// ou
-// caso o valor  seja colocado no console.log, o calculo sairá certo do mesmo jeito, caso não, o valor de b será 1s
-function multiply4(a, b = 1) {
-  //b pode ser b = a também
-  return a * b;
-}
-console.log(multiply4(5, 4)); //pode colocar undefined no lugar de b também
+// // ou
+// // caso o valor  seja colocado no console.log, o calculo sairá certo do mesmo jeito, caso não, o valor de b será 1s
+// function multiply4(a, b = 1) {
+//   //b pode ser b = a também
+//   return a * b;
+// }
+// console.log(multiply4(5, 4)); //pode colocar undefined no lugar de b também
 
-// ou ainda
-// math. ramdom retorna um número aleatório / nunca será o mesmo valor
-//isso é uma lazy valuation
-function randomNumber() {
-  console.log("gerando um radom number");
+// // ou ainda
+// // math. ramdom retorna um número aleatório / nunca será o mesmo valor
+// //isso é uma lazy valuation
+// function randomNumber() {
+//   console.log("gerando um radom number");
 
-  return Math.random() * 10;
-}
+//   return Math.random() * 10;
+// }
 
-function multiply5(a, b = randomNumber()) {
-  return a * b;
-}
-console.log(multiply5(5));
-console.log(multiply5(5));
-
-
-//rest e spread operator
-//exemplo de como era feito determinadas soluções antes do ES6
-//com esse método é possivel somar qualque quantidade de números citados no console.log
-function sum(a, b) {
-  var value = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    value += arguments[i];
-  }
-  return value;
-}
-console.log(sum(5, 5, 5, 3, 2));
-
-//exemplo com ES6 / rest operator , sempre com ...
-// com o rest o código fica mais llimpo
-function sum2(...args) {
-  // caso seja adicionado outros elementos, o args fica com o que sobrar; exemplo (a, b, ...args)
-
-  return args.reduce((acc, value) => acc + value, 0);
-}
-
-console.log(sum2(5, 5, 5, 3, 2));
+// function multiply5(a, b = randomNumber()) {
+//   return a * b;
+// }
+// console.log(multiply5(5));
+// console.log(multiply5(5));
 
 
+// //rest e spread operator
+// //exemplo de como era feito determinadas soluções antes do ES6
+// //com esse método é possivel somar qualque quantidade de números citados no console.log
+// function sum(a, b) {
+//   var value = 0;
+//   for (var i = 0; i < arguments.length; i++) {
+//     value += arguments[i];
+//   }
+//   return value;
+// }
+// console.log(sum(5, 5, 5, 3, 2));
 
-const str = 'digital Innovation one';
-// quebra a fraze e separa letra por letra
-function logArgs(...args) {
-    console.log(args);
-}
-logArgs(...str);
+// //exemplo com ES6 / rest operator , sempre com ...
+// // com o rest o código fica mais llimpo
+// function sum2(...args) {
+//   // caso seja adicionado outros elementos, o args fica com o que sobrar; exemplo (a, b, ...args)
+
+//   return args.reduce((acc, value) => acc + value, 0);
+// }
+
+// console.log(sum2(5, 5, 5, 3, 2));
 
 
-//faz com que cada item do array seja um item da function
-const arr = [1, 2, 3, 4];
-function logArgs2(a, b, c) {
-    console.log(a,b,c);
-}
-logArgs2(...arr);
 
-// o spreaad operator pode ser usado para construir arrays
-// .concat serve para concatenar
-const arr2 =arr.concat([5, 6, 7]);
- console.log(arr2);
+// const str = 'digital Innovation one';
+// // quebra a fraze e separa letra por letra
+// function logArgs(...args) {
+//     console.log(args);
+// }
+// logArgs(...str);
 
 
-// outro método
-//com spread operator
-const arr3 = [...arr, 5, 6, 7];
+// //faz com que cada item do array seja um item da function
+// const arr = [1, 2, 3, 4];
+// function logArgs2(a, b, c) {
+//     console.log(a,b,c);
+// }
+// logArgs2(...arr);
 
-console.log(arr3);
+// // o spreaad operator pode ser usado para construir arrays
+// // .concat serve para concatenar
+// const arr2 =arr.concat([5, 6, 7]);
+//  console.log(arr2);
+
+
+// // outro método
+// //com spread operator
+// const arr3 = [...arr, 5, 6, 7];
+
+// console.log(arr3);
+
+
+
+// aula dia 13/02/2022
+
+// array normal
+var arr = ['apple', 'Banana', 'Orange', ['tomato']];
+
+var apple = arr[0];
+var Banana = arr[1];
+var Orange = arr[2];
+var tomato = arr[3][0];
+
+
+// Destructuring Aassigment, um jeito maais fácil de fazer o array acima, mas com o mesmo  resultado;
+// destruction é bom, mas pode quebrar o codigo caso não seja bem executado com outro array dentro de um array, por exemplo, se a variavel [tomato2] não tiver o valor [tomato] para receber, ocorrerá erro, caso seja outra variavel que esteja apenas dentro de um unico array, recebera o valor undefined.
+var [apple2, banana2, Orange2, [tomato2]] = ['apple', 'Banana', 'Orange', ['tomato']];
+
+console.log(tomato2);
+
+
+// complementos da função console
+
+// .log mostra retornos no console do navegador
+console.log('texto');
+
+// .warn retorna um alerta
+console.warn('texto amarelo como alerta');
+
+// .error
+console.error('texto vermelho com significado de erro');
+
+// .trace
+console.trace();
+
+// .group
+console.group('my group');
+// itens do grupo
+console.log('info inside group');
+console.log('more info inside group');
+// .groupEnd
+console.groupEnd('fim do grupo');
+
+
+// .time
+console.time('log time');
+setTimeout(() => {
+ console.timeEnd('log time');   
+});
+
+
+// .table
+
+console.table(['Allysson Ernandes', 'Estuda muito']);
+console.log('%c styled log', 'color: blue;');
